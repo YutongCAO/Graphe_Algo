fc8cd6cf07f1343d09157c543a59da701764b3f7
CHENZhoujing <zhoujingchen1@gmail.com>
2021-11-26 10:26:23 +0100

    26/11/21

--- TP2.py
+++ TP2.py
@@ -2,14 +2,10 @@ import numpy as np
 import networkx as nx
 from matplotlib import pyplot as plt
 
-X = [0, 1, 2, 3, 4, 5, 6]
-start = [0,0]
-to = [1,2]
-value = [6,2]
-
 go = nx.DiGraph()
-go.add_nodes_from(X)
-for j in range(len(start)):
-     go.add_edges_from([(start[j], to[j], value[j])])
-nx.draw(go, with_labels=True)
+U = [(0, 1, 6), (0, 2, 2), (0, 3, 1), (1, 2, -3), (1, 4, 7), (2, 3, -2), (2, 4, -4), (3, 5, 2), (4, 5, 3), (4, 6, 3),
+     (5, 6, 1)]
+for a,b,w in U:
+    go.add_edge(a, b, weights = w)
+nx.draw(go, with_labels=True,node_color='lightgrey',node_size=600,font_weight='bold')
 plt.show()
