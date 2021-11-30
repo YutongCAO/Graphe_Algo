824207ef3362540c67e8e9ed29f21d605720ea9d
CHENZhoujing <zhoujingchen1@gmail.com>
2021-11-30 12:58:37 +0100

    30/11/21

---
+++ TP2 suite.py
@@ -0,0 +1,28 @@
+import networkx as nx
+from matplotlib import pyplot as plt
+
+print("l’exploitation de gisement pétrolier est un ’minimum spanning tree’")
+
+
+G = nx.Graph()
+U = []
+f = open('arbreCouvrantTP2.txt','r')
+str_list = f.read().split("\n")
+for i in str_list:
+    temp = i.split(" ")
+    tup = ()
+    for j in temp:
+        tup = tup + (float(j),)
+    U.append(tup)
+
+G.add_weighted_edges_from(U)
+nx.draw(G, pos=nx.spring_layout(G), with_labels=True)
+plt.show()
+plt.savefig("minimum_spanning_tree.jpg")
+plt.close()
+sol = nx.minimum_spanning_tree(G)
+print("Arbre couvrant : ", sol.edges)
+val = 0
+for i in sol.edges:
+     val += G[i[0]][i[1]]['weight']
+print('Valeur optimale : ', val)
